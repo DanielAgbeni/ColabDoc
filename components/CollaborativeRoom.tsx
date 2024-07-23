1942085548026ec4b329c8ad6cc8d277582ae2d2
Daniel Agbeni <99095841+DanielAgbeni@users.noreply.github.com>
2024-07-23 19:57:30 +0100

    Add comment

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -7,37 +7,67 @@ import { Editor } from './editor/Editor';
 import ActiveCollaborators from './ActiveCollaborators';
 import { Input } from './ui/input';
 import Image from 'next/image';
+import { updateDocument } from '@/lib/actions/room.actions';
+import Loader from './Loader';
 
 const CollaborativeRoom = ({
 	roomId,
 	roomMetadata,
 	users,
-}: // currentUserType,
-CollaborativeRoomProps) => {
+	currentUserType,
+}: CollaborativeRoomProps) => {
 	const [editing, setEditing] = useState(false);
 	const [loading, setLoading] = useState(false);
 	const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
 	const containerRef = useRef<HTMLDivElement>(null);
 	const inputRef = useRef<HTMLDivElement>(null);
-	const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {};
-	const currentUserType = 'editor';
+	const updateTitleHandler = async (
+		e: React.KeyboardEvent<HTMLInputElement>,
+	) => {
+		if (e.key === 'Enter') {
+			setLoading(true);
+
+			try {
+				if (documentTitle !== roomMetadata.title) {
+					const updatedDocument = await updateDocument(roomId, documentTitle);
+
+					if (updatedDocument) {
+						setEditing(false);
+					}
+				}
+			} catch (error) {
+				console.error(error);
+			}
+
+			setLoading(false);
+		}
+	};
+
 	useEffect(() => {
 		const handleClickOutside = (e: MouseEvent) => {
 			if (
 				containerRef.current &&
 				!containerRef.current.contains(e.target as Node)
-			)
+			) {
 				setEditing(false);
+				updateDocument(roomId, documentTitle);
+			}
 		};
 		document.addEventListener('mousedown', handleClickOutside);
 		return () => {
 			document.removeEventListener('mousedown', handleClickOutside);
 		};
-	}, []);
+	}, [documentTitle, roomId]);
+
+	useEffect(() => {
+		if (editing && inputRef.current) {
+			inputRef.current.focus();
+		}
+	}, [editing]);
 
 	return (
 		<RoomProvider id={roomId}>
-			<ClientSideSuspense fallback={<div>Loading…</div>}>
+			<ClientSideSuspense fallback={<Loader />}>
 				<div className='collaborative-room'>
 					<Header>
 						<div
@@ -57,23 +87,29 @@ CollaborativeRoomProps) => {
 							) : (
 								<>
 									{' '}
-									<p className='document-title'>{documentTitle}</p>
+									<p
+										className='document-title'
+										onDoubleClick={() => setEditing(true)}>
+										{documentTitle}
+									</p>
 								</>
 							)}
-							{currentUserType !== 'editor' && !editing && (
+							{currentUserType === 'editor' && !editing && (
 								<Image
 									src='/assets/icons/edit.svg'
 									alt='edit'
-									className='pointer'
 									width={24}
 									height={24}
 									onClick={() => setEditing(true)}
+									className='cursor-pointer'
 								/>
 							)}
-							{currentUserType === 'editor' && !editing && (
-								<p className='view-only-tag'>View Only</p>
+
+							{currentUserType !== 'editor' && !editing && (
+								<p className='view-only-tag'>View only</p>
 							)}
-							{loading && <p className='text-sm text-gray-400'>Saving...</p>}
+
+							{loading && <p className='text-sm text-gray-400'>saving...</p>}
 						</div>
 						<div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
 							<ActiveCollaborators />
@@ -85,7 +121,10 @@ CollaborativeRoomProps) => {
 							</SignedIn>
 						</div>
 					</Header>
-					<Editor />
+					<Editor
+						roomId={roomId}
+						currentuserType={currentUserType}
+					/>
 				</div>
 			</ClientSideSuspense>
 		</RoomProvider>
